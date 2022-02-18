9696cb16068fd601c7d252fce7e370085ddcc3b3
Felype Henrique <felypehenrique25@gmail.com>
2022-02-18 11:09:09 -0300

    navigation productInfo

--- components/screen/ProductInfo.js
+++ components/screen/ProductInfo.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {View, Text} from 'react-native'
+import {Items} from '../database/Database'
 
-const ProductInfo = () => {
+const ProductInfo = ({route, navigation}) => {
+
+    const {productID} = route.params;
+
+    const [product ,  setProduct] = useState({})
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {
+            getDataFromDB()
+        })
+
+        return unsubscribe;
+    }, [navigation])
+
+    const getDataFromDB = () => {
+        for (let index = 0; index < Items.length; index++){
+            if(Items[index].id == productID){
+
+            }
+        }
+    }
     return(
         <View>
             <Text>ProductInfo</Text>
